e0a9d64ab476091751c9aebe2bec9a78fb2890df
douglas camargo <douglascamargo159@gmail.com>
2020-07-23 15:43:37 -0430

    perfeccionandolo

--- js.js
+++ js.js
@@ -3,8 +3,8 @@
  function validacioon() {
   let datos = {"cedulas":["25000111", "21000222", "22444333", "23222333", "25111222"]};
   let cedula = document.getElementById("cedulaaa").value; //permite el acceso a un elemento por su id
-  var arrays = Object.values(datos)[0];
-  return arrays.includes(cedula)
+
+  return datos.cedulas.includes(cedula)
   } 
   
   function esValido() {
